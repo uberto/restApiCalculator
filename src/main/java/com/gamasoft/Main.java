d1f66ddf76979209e301070f7fd2bfdfe4efa3cd
Uberto Barbini <uberto.gama@gmail.com>
2017-07-07 20:30:28 +0100

    first commit

---
+++ src/main/java/com/gamasoft/Main.java
@@ -0,0 +1,12 @@
+package com.gamasoft;
+
+/**
+ * Created by uberto on 07/07/17.
+ */
+public class Main {
+
+
+    public static void main(String[] args){
+        System.out.println("Bye");
+    }
+}
